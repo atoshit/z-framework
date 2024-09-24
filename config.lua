64028015b0b55af97433dff2cc1e311fe33b9223
Atoshi <atoshit@protonmail.com>
2024-09-24 06:29:21 +0200

    mod: config.lua

--- config.lua
+++ config.lua
@@ -1,11 +1,18 @@
 local Config <const> = {
+    ServerName = "Z Framework",
     Debug = false,
+
+    Color = {
+        rgba = {r = 238, g = 172, b = 1, a = 255}, -- Yellow (https://htmlcolorcodes.com/fr/)
+        hex = "#eeac01" -- Yellow (https://htmlcolorcodes.com/fr/)
+    },
 
     Start = {
         spawn = {x = 0, y = 0, z = 0, h = 0},
-        money = 5000,
-        bank = 5000,
-        item = {}
+        cash = 5000, -- number or nil
+        bank = 5000, -- number or nil
+        item = {}, -- table or nil
+        weapon = nil, -- table or nil
     }
 }
 
