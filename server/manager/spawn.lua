1d2c8b8cd903c80ddb42473baad5fb96bd71a345
Atoshi <atoshit@protonmail.com>
2024-09-26 05:10:23 +0200

    mod: save position

--- server/manager/spawn.lua
+++ server/manager/spawn.lua
@@ -1,23 +1,16 @@
-local playerPositions = {}
-
 Z.Event.Register('z-spawn:loadPlayer', function()
     local source = source
     local license = GetPlayerIdentifierByType(source, 'license')
 
-    if playerPositions[license] then
-        local position = json.decode(playerPositions[license])
-        Z.Event.TriggerClient('z-spawn:loadPlayer', source, position.x, position.y, position.z, position.h)
-        return
-    end
-
     MySQL.fetch('SELECT `position` FROM `players` WHERE `license` = ?', {license}, function(result)
-        local position
         if result[1] then
-            position = json.decode(result[1].position)
-        end
+            local position = json.decode(result[1].position)
 
-        if position then
-            Z.Event.TriggerClient('z-spawn:loadPlayer', source, position.x, position.y, position.z, position.h)
+            if position then
+                Z.Event.TriggerClient('z-spawn:loadPlayer', source, position.x, position.y, position.z, position.h)
+            else
+                Z.Event.TriggerClient('z-spawn:loadPlayer', source, Config.Start.spawn.x, Config.Start.spawn.y, Config.Start.spawn.z, Config.Start.spawn.h)
+            end
         else
             Z.Event.TriggerClient('z-spawn:loadPlayer', source, Config.Start.spawn.x, Config.Start.spawn.y, Config.Start.spawn.z, Config.Start.spawn.h)
         end
@@ -28,24 +21,19 @@ AddEventHandler('playerDropped', function()
     local source = source
     local x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(source)))
     local h = GetEntityHeading(GetPlayerPed(source))
-    local license = GetPlayerIdentifierByType(source, 'license')
 
-    playerPositions[license] = json.encode({x = x, y = y, z = z, h = h})
+    MySQL.execute('UPDATE `players` SET `position` = ? WHERE `license` = ?', {json.encode({x = x, y = y, z = z, h = h}), GetPlayerIdentifierByType(source, 'license')})
 end)
 
 AddEventHandler('onResourceStop', function(resourceName)
     if resourceName == GetCurrentResourceName() then
-        for license, position in pairs(playerPositions) do
-            MySQL.execute('UPDATE `players` SET `position` = ? WHERE `license` = ?', {position, license})
-        end
-    end
-end)
+        for _, playerId in ipairs(GetPlayers()) do
+            local x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(playerId)))
+            local h = GetEntityHeading(GetPlayerPed(playerId))
 
-Ctz.CreateThread(function()
-    while true do
-        for license, position in pairs(playerPositions) do
-            MySQL.execute('UPDATE `players` SET `position` = ? WHERE `license` = ?', {position, license})
+            MySQL.execute('UPDATE `players` SET `position` = ? WHERE `license` = ?', {json.encode({x = x, y = y, z = z, h = h}), GetPlayerIdentifierByType(playerId, 'license')})
         end
-        Ctz.Wait(60000)
+    else
+        return
     end
 end)
